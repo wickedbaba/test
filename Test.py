f00960209581c01bd45b882d12432ccb1d198c79
wickedbaba <54187786+wickedbaba@users.noreply.github.com>
2020-10-28 23:15:13 +0530

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,13 @@
+# for x in range(1, 10, 3):
+#     print(x)
+# for x in range(10):
+#     for y in range(x):
+#         print(y)
+# def replace(email,old_domain,new_domain)
+#     if "@" + old_domain in email :
+#         index = email.index("@" + old_domain)
+#         new_email = email[:index] + "@" + new_domain
+#         return new_email
+name = "Hary"
+number = len(name) * 3
+print("Hello {}, your lucky number is {}".format(name,number))
